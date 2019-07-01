b9da41c3db8f9c872baaaf87e216831b6144ca4e
JayaKrishna Mandivarapu <jayanayana0@gmail.com>
2019-07-01 18:56:05 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 # The command below creates a docker images from the io docker file
-docker build --no-cache -t gcr.io/pupster900/io:latest . 
+docker build --no-cache -t jayanayana/courselabz
 
 # The next commented lines save the docker image
 # docker tag gcr.io/pupster900/io:latest gcr.io/pupster900/io:3.0
 # docker push gcr.io/pupster900/io:latest
-# docker push gcr.io/pupster900/io:3.0
+docker push jayanayana/courselabz:1.0
 
 
 
