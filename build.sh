fa156ebc2bb710ff881d4dee0213452486452c71
JayaKrishna Mandivarapu <jayanayana0@gmail.com>
2019-07-02 00:42:31 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 docker build --no-cache -t jayanayana/courselabz
 
 # The next commented lines save the docker image
-# docker tag gcr.io/pupster900/io:latest gcr.io/pupster900/io:3.0
+docker tag jayanayana/courselabz:tagname
 # docker push gcr.io/pupster900/io:latest
-docker push jayanayana/courselabz:1.0
+docker push jayanayana/courselabz:tagname
 
 
 
